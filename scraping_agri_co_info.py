b0101dc4c62ffdfce3febe06d0fa8a5e631ec321
HaseU-git <hasegawayuta@MacBook-Pro-3.local>
2021-02-19 18:14:21 +0900

    Modify: change the code to read more easily
    - not handle error yet

--- scraping_agri_co_info.py
+++ scraping_agri_co_info.py
@@ -3,47 +3,59 @@ import time
 from selenium import webdriver
 import pandas as pd
 
-BASE = "https://www.be-farmer.jp/experience/intern/search/"
+BASE_URL = "https://www.be-farmer.jp/experience/intern/search/"
+CSV_NAME = "result.csv"
+DIR = "./"
+COMPANY_NUM = 541
+CONTENT_SELECOTR = ".type.type-b dd dd"
+COLUMNS_SELECOTR = ".type.type-b dd d"
+INTERVAL = 1
 
 def create_url_list():
-	url = BASE + "detail/"
-	area_num_list = range(541)
+	url = BASE_URL + "detail/"
+	company_id_list = range(COMPANY_NUM)
 	url_list = []
 	
-	for area_num in area_num_list:
-		query = "?id=" + str(area_num + 2)
+	for company_id in company_id_list:
+		# id=0, id=1は会社が割り当てられていないため
+		query = "?id=" + str(company_id + 2)
 		url = urllib.parse.urljoin(url, query)
 		url_list.append(url)
 
 	return url_list
 
-def create_url_detail(url_list):
-	
-	browser = webdriver.Firefox()
-	index_list = []
+def create_parsed_list(url, selector, driver):
+	ret_list = []
+
+	driver.get(url)
+	res = driver.find_elements_by_css_selector(selector)
+	for el in res:
+		ret_list.append(el.text)
+
+	time.sleep(INTERVAL)
+
+	return ret_list
+
+
+def create_ret_df(url_list):
+	columns_list = []
 	contents_lists = []
-	browser.get(url_list[0])
-	index_res = browser.find_elements_by_css_selector(".type.type-b dd dt")
-	for el in index_res:
-		index_list.append(el.text)
+
+	driver = webdriver.Firefox()
+
+	index_list = create_parsed_list(url_list[0], COLUMNS_SELECOTR, driver)
 
 	for url in url_list:
-		content_list = []
-		browser.get(url)
-		content_res = browser.find_elements_by_css_selector(".type.type-b dd dd")
-		for el in content_res:
-			content_list.append(el.text)
-		contents_lists.append(content_list)
-		time.sleep(1)
+		contents_list = create_parsed_list(url, CONTENT_SELECOTR, driver)
+		contents_lists.append(contents_list)
 
-	df = pd.DataFrame(contents_lists, columns=index_list)
-	#df = df["売上高"].str.z2h
-	#df = df["従業員数"].str.z2h
-	df.to_csv("test.csv")
+	df = pd.DataFrame(contents_lists, columns=columns_list)
+
+	return df
 
 def main_func():
 	url_list = create_url_list()
-	print(url_list)
-	create_url_detail(url_list)
+	df = create_ret_df(url_list)
+	df.to_csv(DIR + CSV_NAME)
 
 main_func()
